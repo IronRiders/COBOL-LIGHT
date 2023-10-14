ae217127201f7b376727846ee589d92856778db4
ampersanded <salvobonsma@gmail.com>
2023-10-13 22:24:45 -0700

    RotateToCommand done

---
+++ src/main/java/team/ironriders/commands/RotateToCommand.java
@@ -0,0 +1,46 @@
+package team.ironriders.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.ironriders.constants.Constants;
+import team.ironriders.subsystems.DriveSubsystem;
+
+
+public class RotateToCommand extends CommandBase {
+    private final double target;
+    private final DriveSubsystem drive;
+
+    public RotateToCommand(double target, DriveSubsystem drive) {
+        this.target = target;
+        this.drive = drive;
+
+        addRequirements(drive);
+    }
+
+    @Override
+    public void execute() {
+        // test dividing error
+
+        drive.setChassisSpeeds(
+                0,
+                0,
+                MathUtil.clamp(error() / 5, -1, 1) * Constants.DRIVE_SPEED_SLOW,
+                false
+        );
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(error()) < 1.5;
+    }
+
+    private double error() {
+        double error = (target - drive.pigeon.getAngle() % 360) % 360;
+        if (error > 180) {
+            error -= 360;
+        } else if (error < -180) {
+            error += 360;
+        }
+        return error;
+    }
+}
